b9deaecc5af59632cded6cf1b2b0cd758fcb746f
Gert Drapers <gert.drapers@live.com>
2022-10-12 12:29:42 +0200

    GetGraph basic filtering

--- pkg/types/objectDependency.go
+++ pkg/types/objectDependency.go
@@ -19,8 +19,8 @@ type ObjectDependency struct {
 
 type ObjectDependencies []*ObjectDependency
 
-func GetGraph(ctx context.Context, i *dsr.GetGraphRequest, store *boltdb.BoltDB, opts ...boltdb.Opts) (ObjectDependencies, error) {
-	if ok, err := ObjectIdentifier.Validate(i.Anchor); !ok {
+func GetGraph(ctx context.Context, req *dsr.GetGraphRequest, store *boltdb.BoltDB, opts ...boltdb.Opts) (ObjectDependencies, error) {
+	if ok, err := ObjectIdentifier.Validate(req.Anchor); !ok {
 		return []*ObjectDependency{}, err
 	}
 
@@ -31,7 +31,7 @@ func GetGraph(ctx context.Context, i *dsr.GetGraphRequest, store *boltdb.BoltDB,
 	}
 
 	// resolve anchor object
-	anchor, err := GetObject(ctx, i.Anchor, store, opts...)
+	anchor, err := GetObject(ctx, req.Anchor, store, opts...)
 	if err != nil {
 		return []*ObjectDependency{}, err
 	}
@@ -40,6 +40,8 @@ func GetGraph(ctx context.Context, i *dsr.GetGraphRequest, store *boltdb.BoltDB,
 	if deps, err = sc.getObjectDependencies(anchor.Id, 0, []string{anchor.Id}, deps); err != nil {
 		return []*ObjectDependency{}, err
 	}
+
+	deps, err = filterObjectDependencies(req, deps)
 
 	sort.Slice(deps, func(i, j int) bool {
 		if deps[i].Depth < deps[j].Depth {
@@ -94,4 +96,56 @@ func (sc *StoreContext) getObjectDependencies(anchorID string, depth int32, path
 		}
 	}
 	return deps, nil
+}
+
+type filter func(*ObjectDependency) bool
+
+func filterObjectDependencies(req *dsr.GetGraphRequest, deps ObjectDependencies) (ObjectDependencies, error) {
+	filters := []filter{}
+
+	if req.Object != nil && req.Object.Id != nil && ID.IsValidIfSet(req.Object.GetId()) {
+		filters = append(filters, func(od *ObjectDependency) bool {
+			return od.ObjectId == req.Object.GetId()
+		})
+	}
+	if req.Object != nil && req.Object.Type != nil && *req.Object.Type != "" {
+		filters = append(filters, func(od *ObjectDependency) bool {
+			return strings.EqualFold(od.ObjectType, req.Object.GetType())
+		})
+	}
+
+	if req.Subject != nil && req.Subject.Id != nil && ID.IsValidIfSet(req.Subject.GetId()) {
+		filters = append(filters, func(od *ObjectDependency) bool {
+			return od.SubjectId == req.Subject.GetId()
+		})
+	}
+	if req.Subject != nil && req.Subject.Type != nil && *req.Subject.Type != "" {
+		filters = append(filters, func(od *ObjectDependency) bool {
+			return strings.EqualFold(od.SubjectType, req.Subject.GetType())
+		})
+	}
+
+	if req.Relation != nil && req.Relation.Name != nil && *req.Relation.Name != "" {
+		filters = append(filters, func(od *ObjectDependency) bool {
+			return strings.EqualFold(od.Relation, req.Relation.GetName())
+		})
+	}
+
+	results := []*ObjectDependency{}
+	for i := 0; i < len(deps); i++ {
+		if includeObjectDependency(deps[i], filters) {
+			results = append(results, deps[i])
+		}
+	}
+
+	return results, nil
+}
+
+func includeObjectDependency(dep *ObjectDependency, filters []filter) bool {
+	for _, fn := range filters {
+		if !fn(dep) {
+			return false
+		}
+	}
+	return true
 }
