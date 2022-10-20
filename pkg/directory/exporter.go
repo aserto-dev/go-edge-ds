e707c3ea6e970a1a02ae19a7edcf939e31c02d5d
Gert Drapers <gert.drapers@live.com>
2022-10-19 22:01:06 -0700

    exporter

--- pkg/directory/exporter.go
+++ pkg/directory/exporter.go
@@ -1,7 +1,228 @@
 package directory
 
-import dse "github.com/aserto-dev/go-directory/aserto/directory/exporter/v2"
+import (
+	dsc "github.com/aserto-dev/go-directory/aserto/directory/common/v2"
+	dse "github.com/aserto-dev/go-directory/aserto/directory/exporter/v2"
+	"github.com/aserto-dev/go-edge-ds/pkg/boltdb"
+	"github.com/aserto-dev/go-edge-ds/pkg/types"
+)
 
-func (s *Directory) Export(*dse.ExportRequest, dse.Exporter_ExportServer) error {
+func (s *Directory) Export(req *dse.ExportRequest, stream dse.Exporter_ExportServer) (err error) {
+	logger := s.logger.With().Str("method", "Export").Interface("req", req).Logger()
+
+	txOpt, cleanup, err := s.store.ReadTxOpts()
+	if err != nil {
+		return err
+	}
+	defer func() {
+		cErr := cleanup()
+		if cErr != nil {
+			err = cErr
+		}
+	}()
+
+	sc := types.StoreContext{Context: stream.Context(), Store: s.store, Opts: []boltdb.Opts{txOpt}}
+
+	if req.Options&uint32(dse.Option_OPTION_METADATA_OBJECT_TYPES) != 0 {
+		if err := exportObjectTypes(&sc, stream); err != nil {
+			logger.Error().Err(err).Msg("export_object_types")
+			return err
+		}
+	}
+
+	if req.Options&uint32(dse.Option_OPTION_METADATA_PERMISSIONS) != 0 {
+		if err := exportPermissions(&sc, stream); err != nil {
+			logger.Error().Err(err).Msg("export_permissions")
+			return err
+		}
+	}
+
+	if req.Options&uint32(dse.Option_OPTION_METADATA_RELATION_TYPES) != 0 {
+		if err := exportRelationTypes(&sc, stream); err != nil {
+			logger.Error().Err(err).Msg("export_relation_types")
+			return err
+		}
+	}
+
+	if req.Options&uint32(dse.Option_OPTION_DATA_OBJECTS) != 0 {
+		if err := exportObjects(&sc, stream); err != nil {
+			logger.Error().Err(err).Msg("export_objects")
+			return err
+		}
+	}
+
+	if req.Options&uint32(dse.Option_OPTION_DATA_RELATIONS) != 0 {
+		if err := exportRelations(&sc, stream); err != nil {
+			logger.Error().Err(err).Msg("export_relations")
+			return err
+		}
+	}
+
+	return nil
+}
+
+func exportObjectTypes(sc *types.StoreContext, stream dse.Exporter_ExportServer) error {
+	page := &types.PaginationRequest{PaginationRequest: &dsc.PaginationRequest{
+		Size:  100,
+		Token: "",
+	}}
+
+	for {
+		objTypes, pageResp, err := sc.GetObjectTypes(page)
+		if err != nil {
+			return err
+		}
+
+		for _, objType := range objTypes {
+
+			if err := stream.Send(&dse.ExportResponse{
+				Msg: &dse.ExportResponse_ObjectType{
+					ObjectType: objType.ObjectType,
+				},
+			}); err != nil {
+				return err
+			}
+		}
+
+		if pageResp.NextToken == "" {
+			break
+		}
+
+		page.Token = pageResp.NextToken
+	}
+	return nil
+}
+
+func exportPermissions(sc *types.StoreContext, stream dse.Exporter_ExportServer) error {
+	page := &types.PaginationRequest{PaginationRequest: &dsc.PaginationRequest{
+		Size:  100,
+		Token: "",
+	}}
+
+	for {
+		permissions, pageResp, err := sc.GetPermissions(page)
+		if err != nil {
+			return err
+		}
+
+		for _, permission := range permissions {
+
+			if err := stream.Send(&dse.ExportResponse{
+				Msg: &dse.ExportResponse_Permission{
+					Permission: permission.Permission,
+				},
+			}); err != nil {
+				return err
+			}
+		}
+
+		if pageResp.NextToken == "" {
+			break
+		}
+
+		page.Token = pageResp.NextToken
+	}
+	return nil
+}
+
+func exportRelationTypes(sc *types.StoreContext, stream dse.Exporter_ExportServer) error {
+	page := &types.PaginationRequest{PaginationRequest: &dsc.PaginationRequest{
+		Size:  100,
+		Token: "",
+	}}
+
+	for {
+		relTypes, pageResp, err := sc.GetRelationTypes(&types.ObjectTypeIdentifier{
+			ObjectTypeIdentifier: &dsc.ObjectTypeIdentifier{},
+		}, page)
+		if err != nil {
+			return err
+		}
+
+		for _, relType := range relTypes {
+
+			if err := stream.Send(&dse.ExportResponse{
+				Msg: &dse.ExportResponse_RelationType{
+					RelationType: relType.RelationType,
+				},
+			}); err != nil {
+				return err
+			}
+		}
+
+		if pageResp.NextToken == "" {
+			break
+		}
+
+		page.Token = pageResp.NextToken
+	}
+	return nil
+}
+
+func exportObjects(sc *types.StoreContext, stream dse.Exporter_ExportServer) error {
+	page := &types.PaginationRequest{PaginationRequest: &dsc.PaginationRequest{
+		Size:  100,
+		Token: "",
+	}}
+
+	for {
+		objects, pageResp, err := sc.GetObjects(&types.ObjectTypeIdentifier{
+			ObjectTypeIdentifier: &dsc.ObjectTypeIdentifier{},
+		}, page)
+		if err != nil {
+			return err
+		}
+
+		for _, obj := range objects {
+
+			if err := stream.Send(&dse.ExportResponse{
+				Msg: &dse.ExportResponse_Object{
+					Object: obj.Object,
+				},
+			}); err != nil {
+				return err
+			}
+		}
+
+		if pageResp.NextToken == "" {
+			break
+		}
+
+		page.Token = pageResp.NextToken
+	}
+	return nil
+}
+
+func exportRelations(sc *types.StoreContext, stream dse.Exporter_ExportServer) error {
+	page := &types.PaginationRequest{PaginationRequest: &dsc.PaginationRequest{
+		Size:  100,
+		Token: "",
+	}}
+
+	for {
+		relations, pageResp, err := sc.GetRelations(&types.RelationIdentifier{
+			RelationIdentifier: &dsc.RelationIdentifier{},
+		}, page)
+		if err != nil {
+			return err
+		}
+
+		for _, rel := range relations {
+
+			if err := stream.Send(&dse.ExportResponse{
+				Msg: &dse.ExportResponse_Relation{
+					Relation: rel.Relation,
+				},
+			}); err != nil {
+				return err
+			}
+		}
+
+		if pageResp.NextToken == "" {
+			break
+		}
+
+		page.Token = pageResp.NextToken
+	}
 	return nil
 }
