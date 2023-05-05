70f649563940619bfa62dee0dce0978b8bde2ac4
Gert Drapers <gert.drapers@live.com>
2023-05-05 15:04:44 +0200

    refactor db code

---
+++ pkg/boltdb/path.go
@@ -0,0 +1,11 @@
+package boltdb
+
+var (
+	SystemPath        = []string{"_system"}
+	ObjectTypesPath   = []string{"object_types"}
+	PermissionsPath   = []string{"permissions"}
+	RelationTypesPath = []string{"relation_types"}
+	ObjectsPath       = []string{"objects"}
+	RelationsSubPath  = []string{"relations_sub"}
+	RelationsObjPath  = []string{"relations_obj"}
+)
