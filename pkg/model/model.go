5fcc512d6dd55556e72de14bf01615a68e74c02f
Gert Drapers <gert.drapers@live.com>
2023-05-17 14:03:11 -0700

    wip

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,272 +1,87 @@
 package model
 
-import (
-	"context"
-	"os"
+import "io"
 
-	dsc "github.com/aserto-dev/go-directory/aserto/directory/common/v2"
-	"github.com/aserto-dev/go-edge-ds/pkg/bdb"
-	bolt "go.etcd.io/bbolt"
-	"gopkg.in/yaml.v2"
+type Operation int
+
+const (
+	None         Operation = iota // NONE
+	Union                         // OR
+	Intersection                  // AND
+	Exclusion                     // NOT (slot 0 = base, slot 1 = subtraction)
 )
 
-// model contains type system related items
-
 type Model struct {
-	Relations   map[string][]string
-	Permissions map[string]struct{}
+	Name          string        `yaml:"name" json:"name"`
+	SchemaVersion int           `yaml:"schema_version" json:"schema_version"`
+	ObjectTypes   []*ObjectType `yaml:"object_types,omitempty" json:"object_types,omitempty"`
 }
 
-func NewResolver() (*Model, error) {
-	model := Model{
-		Relations:   map[string][]string{},
-		Permissions: map[string]struct{}{},
-	}
-	return &model, nil
+type ObjectType struct {
+	Name        string            `yaml:"name" json:"name"`
+	Relations   []*RelationType   `yaml:"relations,omitempty" json:"relations,omitempty"`
+	Permissions []*PermissionType `yaml:"permissions,omitempty" json:"permissions,omitempty"`
 }
 
-func (r *Model) Update(ctx context.Context, tx *bolt.Tx) error {
-	page := &dsc.PaginationRequest{Size: 100}
-
-	// get object types
-	for {
-		results, next, err := bdb.List[dsc.ObjectType](ctx, tx, bdb.ObjectTypesPath, page)
-		if err != nil {
-			return err
-		}
-
-		for _, objType := range results {
-			_, ok := r.Relations[objType.Name]
-			if !ok {
-				r.Relations[objType.Name] = []string{}
-			}
-		}
-
-		if next.GetNextToken() == "" {
-			break
-		}
-
-		page.Token = next.GetNextToken()
-	}
-
-	// get relation types + unions
-	for {
-		results, next, err := bdb.List[dsc.RelationType](ctx, tx, bdb.RelationTypesPath, page)
-		if err != nil {
-			return err
-		}
-
-		for _, relType := range results {
-			_, ok := r.Relations[relType.ObjectType+"/"+relType.Name]
-			if !ok {
-				r.Relations[relType.ObjectType+"/"+relType.Name] = relType.Unions
-			}
-		}
-
-		// get permissions
-		for _, relType := range results {
-			for _, u := range relType.Unions {
-				_ = u
-				for _, p := range relType.Permissions {
-					_, ok := r.Permissions[relType.ObjectType+"/"+relType.Name+"/"+p]
-					if !ok {
-						r.Permissions[relType.ObjectType+"/"+relType.Name+"/"+p] = struct{}{}
-					}
-				}
-			}
-		}
-
-		if next.GetNextToken() == "" {
-			break
-		}
-
-		page.Token = next.GetNextToken()
-	}
-
-	return nil
+type RelationType struct {
+	Name       string    `yaml:"name" json:"name"`
+	Definition string    `yaml:"definition" json:"definition"`
+	Operation  Operation `yaml:"operation" json:"operation"`
+	Relations  []string  `yaml:"relations,omitempty" json:"relations,omitempty"`
 }
 
-func (r *Model) ObjectTypeExists(objectType string) bool {
-	_, ok := r.Relations[objectType]
-	return ok
+type PermissionType struct {
+	Name        string    `yaml:"name" json:"name"`
+	Definition  string    `yaml:"definition" json:"definition"`
+	Operation   Operation `yaml:"operation" json:"operation"`
+	Permissions []string  `yaml:"permissions,omitempty" json:"permissions,omitempty"`
 }
 
-func (r *Model) RelationExists(objectType, relation string) bool {
-	_, ok := r.Relations[objectType+"/"+relation]
-	return ok
+func (m *Model) ObjectTypeExists(objectType string) bool {
+	return false
 }
 
-func (r *Model) PermissionExists(objectType, relation, permission string) bool {
-	_, ok := r.Permissions[objectType+"/"+relation+"/"+permission]
-	return ok
+func (m *Model) RelationExists(objectType, relation string) bool {
+	return false
 }
 
-func (r *Model) ResolveRelation(objectType, relation string) ([]string, error) {
+func (m *Model) PermissionExists(objectType, permission string) bool {
+	return false
+}
+
+func (m *Model) ResolveRelation(objectType, relation string) ([]string, error) {
 	return []string{}, nil
 }
 
-func (r *Model) ResolvePermission(objectType, permission string) ([]string, error) {
+func (m *Model) ResolvePermission(objectType, permission string) ([]string, error) {
 	return []string{}, nil
 }
 
-type Relation map[string][]string
-
-type ObjectRelation map[string]Relation
-
-type Manifest struct {
-	SchemaVersion string `yaml:"schema_version,omitempty" json:"schema_version,omitempty"`
+// Read model instance from manifest.
+type Reader interface {
+	Read(io.Reader) (*Model, error)
 }
 
-type ObjectTypes map[string]ObjectRelation
-
-func Open(filename string) (*Model, error) {
-	buf, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	// manifest := make(ObjectTypes, 0)
-	m := yaml.MapSlice{}
-	if err := yaml.Unmarshal(buf, &m); err != nil {
-		return nil, err
-	}
-
-	// manifestEntriesWithUnions := make(map[string]map[string][]string, 0)
-	// permissions := make(map[string]bool, 0)
-
-	// for objectType, manifestEntry := range manifest {
-	// 	objType := &dsc.ObjectType{
-	// 		Name: objectType,
-	// 	}
-
-	// 	for relationType, data := range manifestEntry {
-	// 		// at first we create relation types that don't have unions
-	// 		if len(data["union"]) > 0 {
-	// 			manifestEntriesWithUnions[relationType] = data
-	// 		} else {
-	// 			err := c.setPermissions(data["permissions"], permissions)
-	// 			if err != nil {
-	// 				return errors.Wrapf(err, "failed to set permissions for relation %s", relationType)
-	// 			}
-
-	// 			req := &writer.SetRelationTypeRequest{
-	// 				RelationType: &v2.RelationType{
-	// 					Name:        relationType,
-	// 					Permissions: data["permissions"],
-	// 					ObjectType:  objectType,
-	// 				}}
-	// 			_, err = c.Writer.SetRelationType(ctx, req)
-	// 			if err != nil {
-	// 				return errors.Wrapf(err, "failed to set relation type %s", relationType)
-	// 			}
-	// 		}
-	// 	}
-
-	// 	for relationType, data := range manifestEntriesWithUnions {
-	// 		err := c.setPermissions(data["permissions"], permissions)
-	// 		if err != nil {
-	// 			return errors.Wrapf(err, "failed to set permissions for relation %s", relationType)
-	// 		}
-
-	// 		req := &writer.SetRelationTypeRequest{
-	// 			RelationType: &v2.RelationType{
-	// 				Name:        relationType,
-	// 				Permissions: data["permissions"],
-	// 				ObjectType:  objectType,
-	// 				Unions:      data["union"],
-	// 			}}
-	// 		_, err = c.Writer.SetRelationType(ctx, req)
-	// 		if err != nil {
-	// 			return errors.Wrapf(err, "failed to set relation type %s", relationType)
-	// 		}
-	// 	}
-	// }
-	return &Model{}, nil
+// Write model instance to manifest.
+type Writer interface {
+	Write(io.Writer, *Model) error
 }
 
-// func (c *Client) Load(ctx context.Context, file string) error {
-// 	yfile, err := os.ReadFile(file)
+// Load model instance from directory store.
+type Loader interface {
+	Load() (*Model, error)
+}
 
-// 	if err != nil {
-// 		return err
-// 	}
+// Save model instance to directory store.
+type Saver interface {
+	Save(*Model) error
+}
 
-// 	manifestData := make(Manifest, 0)
+type Object struct {
+	Relations map[string]Relation
+}
 
-// 	err = yaml.Unmarshal(yfile, &manifestData)
-
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	manifestEntriesWithUnions := make(map[string]map[string][]string, 0)
-// 	permissions := make(map[string]bool, 0)
-
-// 	for objectType, manifestEntry := range manifestData {
-// 		req := &writer.SetObjectTypeRequest{
-// 			ObjectType: &v2.ObjectType{
-// 				Name: objectType,
-// 			}}
-// 		_, err := c.Writer.SetObjectType(ctx, req)
-// 		if err != nil {
-// 			return errors.Wrapf(err, "failed to set object type %s", objectType)
-// 		}
-// 		for relationType, data := range manifestEntry {
-// 			// at first we create relation types that don't have unions
-// 			if len(data["union"]) > 0 {
-// 				manifestEntriesWithUnions[relationType] = data
-// 			} else {
-// 				err := c.setPermissions(data["permissions"], permissions)
-// 				if err != nil {
-// 					return errors.Wrapf(err, "failed to set permissions for relation %s", relationType)
-// 				}
-
-// 				req := &writer.SetRelationTypeRequest{
-// 					RelationType: &v2.RelationType{
-// 						Name:        relationType,
-// 						Permissions: data["permissions"],
-// 						ObjectType:  objectType,
-// 					}}
-// 				_, err = c.Writer.SetRelationType(ctx, req)
-// 				if err != nil {
-// 					return errors.Wrapf(err, "failed to set relation type %s", relationType)
-// 				}
-// 			}
-// 		}
-
-// 		for relationType, data := range manifestEntriesWithUnions {
-// 			err := c.setPermissions(data["permissions"], permissions)
-// 			if err != nil {
-// 				return errors.Wrapf(err, "failed to set permissions for relation %s", relationType)
-// 			}
-
-// 			req := &writer.SetRelationTypeRequest{
-// 				RelationType: &v2.RelationType{
-// 					Name:        relationType,
-// 					Permissions: data["permissions"],
-// 					ObjectType:  objectType,
-// 					Unions:      data["union"],
-// 				}}
-// 			_, err = c.Writer.SetRelationType(ctx, req)
-// 			if err != nil {
-// 				return errors.Wrapf(err, "failed to set relation type %s", relationType)
-// 			}
-// 		}
-// 	}
-
-// 	return nil
-// }
-
-// func (c *Client) setPermissions(permissions []string, alreadyAddedPerms map[string]bool) error {
-// 	for _, perm := range permissions {
-// 		if !alreadyAddedPerms[perm] {
-// 			req := &writer.SetPermissionRequest{Permission: &v2.Permission{Name: perm}}
-// 			_, err := c.Writer.SetPermission(context.Background(), req)
-// 			if err != nil {
-// 				return err
-// 			}
-// 			alreadyAddedPerms[perm] = true
-// 		}
-// 	}
-// 	return nil
-// }
+type Relation struct {
+	Union       []string
+	Permissions []string
+}
--- pkg/model/v2/model_test.go
+++ pkg/model/v2/model_test.go
@@ -1,21 +1,21 @@
-package v3_test
+package v2_test
 
 import (
 	"os"
 	"testing"
 
-	v3 "github.com/aserto-dev/go-edge-ds/pkg/model/v3"
+	v2 "github.com/aserto-dev/go-edge-ds/pkg/model/v2"
 	"github.com/stretchr/testify/require"
 )
 
 func TestLoadManifest(t *testing.T) {
-	m3 := v3.ModelV3{}
+	m2 := v2.ModelV2{}
 
 	r, err := os.Open("./manifests/example.yaml")
 	require.NoError(t, err)
 	require.NotNil(t, r)
 
-	m, err := m3.Read(r)
+	m, err := m2.Read(r)
 	require.NoError(t, err)
 	require.NotNil(t, m)
 }
